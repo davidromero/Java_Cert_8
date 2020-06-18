a56cf41265897d76e0c272c1a473232621f25945
davidromero <d-romero@live.com>
2020-06-17 18:32:08 -0600

    Advance Flow Control

---
+++ src/Advance_Flow_Control.java
@@ -0,0 +1,57 @@
+public class Advance_Flow_Control {
+
+    public static void main(String[] args){
+
+        int[][] myComplexArray = {{5,2,1,3},{3,9,8,9},{5,7,12,7}};
+        OUTER_LOOP: for(int[] mySimpleArray : myComplexArray){
+            INNER_LOOP: for(int i=0; i < mySimpleArray.length; i++){
+                System.out.print(mySimpleArray[i]+"\t");
+            }
+            System.out.println();
+        }
+
+        int x = 20;
+        while(x>0){
+            do{
+                x -= 2;
+            } while(x>5);
+            x--;
+            System.out.print(x+"\t");
+        }
+
+        System.out.println("Break Statment");
+        break_statment();
+    }
+
+    public static void break_statment(){
+        int[][] list = {{1,13,5},{1,2,5},{2,7,2}};
+        int searchValue = 2;
+        int positionX = -1;
+        int positionY = -1;
+        PARENT_LOOP: for(int i=0; i<list.length; i++) {
+            for(int j=0; j<list[i].length; j++) {
+                if(list[i][j]==searchValue) {
+                    positionX = i;
+                    positionY = j;
+                    break PARENT_LOOP;
+                }
+            }
+        }
+        //TODO
+        if(positionX==-1 || positionY==-1) {
+            System.out.println("Value "+searchValue+" not found");
+        } else {
+            System.out.println("Value "+searchValue+" found at: " +
+                    "("+positionX+","+positionY+")");
+        }
+
+        FIRST_CHAR_LOOP: for (int a = 1; a <= 4; a++) {
+            for (char x = 'a'; x <= 'c'; x++) {
+                if (a == 2 || x == 'b')
+                    continue;
+                    //continue FIRST_CHAR_LOOP;
+                System.out.print(" " + a + x);
+            }
+        }
+    }
+}
