b587d2361abbb87bab417d6617afc6ebcbf7ad2e
davidromero <d-romero@live.com>
2020-08-09 01:59:48 -0600

    Exceptions

---
+++ src/ExceptionExample.java
@@ -0,0 +1,16 @@
+public class ExceptionExample {
+
+    public static void main(String[] args){
+        
+
+        try{
+            fall();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    static void fall() throws Exception{
+        throw new Exception("Ow! I fell.");
+    }
+}
